2a57b5b65138b2b4e04a3847e998cbd3832cbb06
Foxus Team <team@foxus.com>
2023-02-09 12:58:14 +1300

    Add favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 html_logo = 'foxus-logo-favicon.webp'
+html_favicon = 'foxus-logo-favicon.webp'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
